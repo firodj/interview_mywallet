0aedf13ac2bb4e826c6686dae5c1ce612daf1c83
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-09-22 17:33:32 +0700

    prefix api routes with /api/v1, and force json format include when exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::API
+  before_action :set_default_response_format
+
   class UserNotAuthorized < StandardError; end
 
   rescue_from UserNotAuthorized, with: :user_not_authorized
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-  rescue_from ActiveRecord::InvalidForeignKey, with: :server_error
-  rescue_from ActionController::ParameterMissing, with: :param_error
 
   attr_reader :current_user
   attr_reader :is_admin
@@ -29,19 +28,11 @@ class ApplicationController < ActionController::API
       end
     end
 
-    def record_not_found(exception)
-      render json: { message: exception.message }, status: :not_found
-    end
-
     def user_not_authorized
       render json: { message: "You are not authorized to access this resource" }, status: :unauthorized
     end
 
-    def server_error(exception)
-      render json: { :error => exception.message }, :status => :internal_server_error
-    end
-
-    def param_error(exception)
-      render json: { :error => exception.message }, :status => :bad_request
+    def set_default_response_format
+      request.format = :json
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :transactions
-  resources :wallets
-  resources :stocks
-  resources :users
-  resources :teams
+  scope '/api/v1' do
+    resources :teams
+    resources :stocks
+    resources :users
+    resources :wallets, only: [:index, :show]
+    resources :transactions
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
