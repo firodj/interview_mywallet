21ba7df34078d9d08fc2fd9011712029aa028883
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-09-22 16:30:43 +0700

    add stocks crud controller test

--- test/controllers/stocks_controller_test.rb
+++ test/controllers/stocks_controller_test.rb
@@ -2,35 +2,43 @@ require "test_helper"
 
 class StocksControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @stock = stocks(:one)
+    @stock = stocks(:y9s)
   end
 
   test "should get index" do
-    get stocks_url, as: :json
+    get stocks_url, headers: { Authorization: "Plain system" }, as: :json
     assert_response :success
   end
 
   test "should create stock" do
     assert_difference("Stock.count") do
-      post stocks_url, params: { stock: { company: @stock.company, name: @stock.name } }, as: :json
+      post stocks_url, params: { stock: { name: "GOTO", company: "GoTo Gojek Tokopedia" } },
+        headers: { Authorization: "Plain system" },
+        as: :json
     end
 
     assert_response :created
   end
 
   test "should show stock" do
-    get stock_url(@stock), as: :json
+    get stock_url(@stock),
+      headers: { Authorization: "Plain system" },
+      as: :json
     assert_response :success
   end
 
   test "should update stock" do
-    patch stock_url(@stock), params: { stock: { company: @stock.company, name: @stock.name } }, as: :json
+    patch stock_url(@stock), params: { stock: { name: "b2b", company: "Business Two Business" } },
+      headers: { Authorization: "Plain system" },
+      as: :json
     assert_response :success
   end
 
   test "should destroy stock" do
     assert_difference("Stock.count", -1) do
-      delete stock_url(@stock), as: :json
+      delete stock_url(@stock),
+        headers: { Authorization: "Plain system" },
+        as: :json
     end
 
     assert_response :no_content
