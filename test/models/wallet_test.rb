f88057fe9700311bc41f6bc72266b97c9260c9b4
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-09-22 15:42:33 +0700

    add testing fot wallet model

--- test/models/wallet_test.rb
+++ test/models/wallet_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class WalletTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create wallet without owner will fail" do
+    wallet = Wallet.new
+    assert_not wallet.save
+  end
+
+  test "create wallet with user as an owner will success" do
+    wallet = Wallet.new
+    wallet.owner = users(:alice)
+    assert wallet.save
+  end
+
+  test "create wallet with team as an owner will success" do
+    wallet = Wallet.new
+    wallet.owner = teams(:yellow)
+    assert wallet.save
+  end
+
+  test "create wallet with stock as an owner will success" do
+    wallet = Wallet.new
+    wallet.owner = stocks(:y9s)
+    assert wallet.save
+  end
 end
