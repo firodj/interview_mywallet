0ce4d504f2e8cacb21389d826c307e120e68d182
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-09-22 14:36:04 +0700

    add testing for model team

--- test/models/team_test.rb
+++ test/models/team_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class TeamTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save team without name will failed" do
+    team = Team.new
+    assert_not team.save
+  end
+
+  test "save team with name will success" do
+    team = Team.new
+    team.name = "Green Light"
+    assert team.save
+  end
 end
