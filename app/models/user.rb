c6f7eae5b9a4ee1e1d911ae57418185db780275e
Fadhil Mandaga <1750918+firodj@users.noreply.github.com>
2024-09-22 14:42:47 +0700

    add testing for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   belongs_to :team, optional: true
 
   validates_presence_of :name, :email
-  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,35 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "save user without name will failed" do
+    user = User.new
+    assert_not user.save
+  end
+
+  test "save user with name and email and no team will success" do
+    user = User.new
+    user.name = "Bob"
+    user.email = "bob@example.com"
+    assert user.save
+  end
+
+  test "save user with malform email will failed" do
+    user = User.new
+    user.name = "Bob"
+    user.email = "example.com"
+    assert_not user.save
+  end
+
+  test "update user with already others email will failed" do
+    user = users(:alice)
+    user.email = "john@example.com"
+    assert_not user.save
+  end
+
+  test "update user with malformed email will failed" do
+    user = users(:alice)
+    user.email = "example.com"
+    assert_not user.save
+  end
+
 end
